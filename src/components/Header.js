eafe4db09895be182683ec0f5417657b348f08d6
Almog <almogbh.l@gmail.com>
2019-02-09 15:09:32 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () => (
+    <header>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+              Logo
+            </div>
+        </nav>
+    </header>
+)
+
+export default Header
